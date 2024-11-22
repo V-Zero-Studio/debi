57a1effd32186756490764467e70955c9daf261b
hotnAny <xiangchen@acm.org>
2024-11-21 20:34:33 -0800

    format cpationing output

--- src/generateService.js
+++ src/generateService.js
@@ -37,9 +37,10 @@ export const generateImages = async (prompt) => {
 
 //
 //
-//
+// finally, return a list of suggested attributes as keywords/phases formatted as follows: {keyword/phase, keyword/phase, ... keyword/phase}
 export const generateTags = async (imgUrls) => {
-  const textPrompt = "first, identify a list of attributes that the images have in common; then, for each common attribute, suggest a different value. finally, return a list of suggested attributes as keywords/phases formatted as follows: {keyword/phase, keyword/phase, ... keyword/phase}.\n\n for example: if identifying that all images show 'male' as the gender attribute, you can suggest 'female' or 'non-binary' as a different value, and return {female, non-binary}";
+  const textPrompt =
+    "detect as many as possible attributes that the images have in common and, for each common attribute, suggest a different value.\n\n for example: if identifying that all images show 'male' as the gender attribute, you can suggest 'female' or 'non-binary' as a different value, and return JavaScript code of an array [{\"attribute\": \"gender\", \"common\": \"male\", \"suggestion\": [\"female\", \"non-binary\"]}]. only return the JavaScript code. do not include other texts or markdown code like '```javascript'";
   const messageContents = [
     {
       type: "text",
@@ -55,13 +56,17 @@ export const generateTags = async (imgUrls) => {
   // console.log(message);
 
   try {
-    console.log("captioning the generated images ...")
+    console.log("captioning the generated images ...");
     const response = await openai.chat.completions.create({
       model: "gpt-4o-mini",
       messages: messages,
     });
 
-    console.log(response.choices[0].message.content);
+    const result = response.choices[0].message.content;
+    console.log(result);
+
+    const tags = JSON.parse(result);
+    console.log(tags);
   } catch (error) {
     console.error("Error captioning images:", error);
     return [];
